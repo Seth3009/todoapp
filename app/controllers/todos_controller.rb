1860f1a1ca47d555e686c5b86b2f66fb984a93d1
Seth <setiadi.seth@gmail.com>
2022-03-23 13:05:35 +0700

    add edit action

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -10,19 +10,31 @@ class TodosController < ApplicationController
         @todo = Todo.find(params[:id])
     end
 
+    def edit
+        @todo = Todo.find(params[:id])
+    end
+
     def create
         @todo = Todo.create(todo_params)
         
-            if @todo.save
-                flash[:notice] ="Todo was successfully created"
-                redirect_to todo_path(@todo)
-            else 
-                format.html { 
-                flash[:errors] = @todo.errors.full_messages               
-                redirect_to 'new'
-                }
-            end
-        
+        if @todo.save
+            redirect_to todo_path(@todo)
+            flash[:notice] = "Todo was created successfully"
+        else
+            redirect_to  new_todo_path
+            flash[:error] = @todo.errors.full_messages
+        end  
+    end
+
+    def update
+        @todo = Todo.find(params[:id])
+        if @todo.update(todo_params)
+            flash[:notice] = "Todo was updated successfully"
+            redirect_to todo_path(@todo)
+        else
+            redirect_to edit_todo_path(@todo)
+            flash[:error] = @todo.errors.full_messages
+        end
     end
 
 
